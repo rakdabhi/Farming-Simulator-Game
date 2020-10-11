10f4001af1d5db87f1e8758536340392337f777a
Rakshit Dabhi <rdabhi3@gatech.edu>
2020-10-11 15:46:43 -0400

    More Added Farmer Tests
    This Farmer tester has new updated tests that test the functionality of the updated Farmer class.

--- UITest/JUNIT-Tests/farmer/FarmerTest.java
+++ UITest/JUNIT-Tests/farmer/FarmerTest.java
@@ -1,6 +1,8 @@
 package farmer;
 
 import exceptions.InsufficientFundsException;
+import exceptions.InsufficientInventorySpaceException;
+import exceptions.SeedChoiceNotFoundException;
 import seed.Seed;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,13 +38,13 @@ public class FarmerTest {
 
     @Test
     public void testInitialConfig() {
-        assertEquals(10000, farmer1.getMoney());
+        assertEquals(200.0, farmer1.getMoney(), 0.01);
         assertEquals(0, farmer1.numOfSeeds());
 
-        assertEquals(5000, farmer2.getMoney());
+        assertEquals(100.0, farmer2.getMoney(), 0.01);
         assertEquals(0, farmer2.numOfSeeds());
 
-        assertEquals(1000, farmer3.getMoney());
+        assertEquals(50.0, farmer3.getMoney(), 0.01);
         assertEquals(0, farmer3.numOfSeeds());
     }
 
@@ -50,31 +52,47 @@ public class FarmerTest {
     @Test
     public void setMoney() {
         farmer1.setMoney("2");
-        assertEquals(5000, farmer1.getMoney());
+        assertEquals(100.0, farmer1.getMoney(), 0.01);
 
         farmer2.setMoney("3");
-        assertEquals(1000, farmer2.getMoney());
+        assertEquals(50.0, farmer2.getMoney(), 0.01);
 
         farmer3.setMoney("1");
-        assertEquals(10000, farmer3.getMoney());
+        assertEquals(200.0, farmer3.getMoney(), 0.01);
+    }
+
+    @Test
+    public void inventoryCapacity() {
+        assertEquals(100, farmer1.getAvailableCapacity());
+        assertEquals(100, farmer1.getInventoryCapacity());
+        assertEquals(50, farmer2.getAvailableCapacity());
+        assertEquals(50, farmer2.getInventoryCapacity());
+        assertEquals(25, farmer3.getAvailableCapacity());
+        assertEquals(25, farmer3.getInventoryCapacity());
+    }
+
+    public void testCapacityAfterAddSeed() {
+        farmer3.setSeedBag(seedArrayList);
+        assertEquals(22, farmer3.getAvailableCapacity());
+        assertEquals(25, farmer3.getInventoryCapacity());
     }
 
     @Test
     public void addMoney() {
-        farmer1.addMoney(652);
-        assertEquals(10652, farmer1.getMoney());
+        farmer1.addMoney(652.51);
+        assertEquals(852.51, farmer1.getMoney(), 0.01);
     }
 
     @Test
     public void negativeAddMoney() {
-        farmer1.addMoney((-500));
-        assertEquals(9500, farmer1.getMoney());
+        farmer1.addMoney((-180.23));
+        assertEquals(19.77, farmer1.getMoney(), 0.01);
     }
 
     @Test
     public void pay() {
-        farmer1.pay(5932);
-        assertEquals(4068, farmer1.getMoney());
+        farmer1.pay(0.01);
+        assertEquals(199.99, farmer1.getMoney(), 0.01);
     }
 
     @Test
@@ -83,6 +101,27 @@ public class FarmerTest {
         farmer1.addSeed(seed1);
         farmer1.addSeed(seed3);
         assertEquals(seedArrayList, farmer1.getSeedBag());
+        assertEquals(97, farmer1.getAvailableCapacity());
+        assertEquals(100, farmer1.getInventoryCapacity());
+
+        Seed potato = new Seed("Potato", 37);
+        farmer2.addSeed(potato);
+        assertEquals(13, farmer2.getAvailableCapacity());
+        assertEquals(50, farmer2.getInventoryCapacity());
+    }
+
+    @Test
+    public void removeSeed() throws SeedChoiceNotFoundException {
+        Seed corn = new Seed("Corn", 10);
+        farmer3.addSeed(corn);
+        assertEquals(corn, farmer3.getSeedBag().get(0));
+        assertEquals(15, farmer3.getAvailableCapacity());
+        assertEquals(25, farmer3.getInventoryCapacity());
+
+        farmer3.removeSeed(new Seed("Corn", 5));
+        assertEquals(5, farmer3.getSeedBag().get(0).getQuantity());
+        assertEquals(20, farmer3.getAvailableCapacity());
+        assertEquals(25, farmer3.getInventoryCapacity());
     }
 
     @Test
@@ -103,5 +142,20 @@ public class FarmerTest {
         farmer2.pay(189633);
         farmer3.pay(4545758);
         farmer1.addMoney(-10000590);
+    }
+
+    @Test(expected = InsufficientInventorySpaceException.class)
+    public void insufficientInventory() throws SeedChoiceNotFoundException {
+        Seed apple = new Seed("Apple", 50);
+        farmer3.addSeed(apple);
+        farmer1.addSeed(new Seed("Apple", 10));
+        farmer1.removeSeed(new Seed("Apple", 20));
+    }
+
+    @Test(expected = SeedChoiceNotFoundException.class)
+    public void removeSeedWithException() throws SeedChoiceNotFoundException {
+        Seed apple = new Seed("Apple", 10);
+        farmer1.addSeed(apple);
+        farmer1.removeSeed(new Seed("Corn", 5));
     }
 }
