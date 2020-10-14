326924f370b09881f0082e6fcd645e7a92860b65
mpanzella3 <mpanzella3@gatech.edu>
2020-10-14 02:21:35 -0400

    Created exception to handle harvesting empty plots

---
+++ UITest/src/exceptions/EmptyPlotException.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+public class EmptyPlotException extends Exception {
+    /**
+     * This constructor creates a new Exception with a predetermined message.
+     */
+    public EmptyPlotException() {
+        super("This plot is empty!");
+    }
+
+    /**
+     * This constructor creates a new Exception with a custom message.
+     * @param message the message provided to the user when the exception is thrown
+     */
+    public EmptyPlotException(String message) {
+        super(message);
+    }
+}
